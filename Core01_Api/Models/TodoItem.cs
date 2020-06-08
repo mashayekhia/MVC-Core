618816810ad6105c8e831d3825afc9d629671b97
Aghil <55014236+mashayekhia@users.noreply.github.com>
2020-06-08 13:04:02 +0430

    Update TodoItem.cs

--- Core01_Api/Models/TodoItem.cs
+++ Core01_Api/Models/TodoItem.cs
@@ -9,4 +9,5 @@ public class TodoItem
     public string Key { get; set; }
     public string Name { get; set; }
     public bool IsComplete { get; set; }
+        public string Name2 { get; set; }
 }
